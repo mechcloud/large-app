c549a7c18efacf2d5153486f11ae8afe1b3c0de8
Shailendra Singh <shailendra_01@outlook.com>
2020-06-20 23:58:04 +0530

    Added external component function.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import externalComponent from '@/utils/external-component';
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -11,12 +13,12 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/module1',
+    name: 'Module A',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => externalComponent(/* webpackChunkName: "module1" */ '/module1/Home.js')
   }
 ]
 
