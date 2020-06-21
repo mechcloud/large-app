2f8d7927a485028e9f4ec0afc15f77d3841178f6
Shailendra Singh <shailendra_01@outlook.com>
2020-06-21 17:04:04 +0530

    Updated module1 refs to plugin1.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/module1">Module A</router-link>
+      <router-link to="/plugin1">Module A</router-link>
     </div>
     <router-view/>
   </div>
