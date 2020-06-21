18ba378b36e3fe61b5fc227699f5903de7adf11c
Shailendra Singh <shailendra_01@outlook.com>
2020-06-21 17:26:05 +0530

    Minor changes.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/plugin1">Module A</router-link>
+      <router-link to="/plugin1">Plugin 1</router-link>
     </div>
     <router-view/>
   </div>
