5fb02c2393ffac41f23705f743dd892a8cd27a48
Shailendra Singh <shailendra_01@outlook.com>
2020-06-21 15:47:51 +0530

    Routes for module1 dynamically.

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,22 @@ import externalComponent from '@/utils/external-component';
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/module1',
-    name: 'Module A',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => externalComponent(/* webpackChunkName: "module1" */ '/module1/Home.5df6d51ae69c1517c7d4.umd.min.js')
-  }
+  // {
+  //   path: '/module1',
+  //   name: 'Module A',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => externalComponent(/* webpackChunkName: "module1" */ '/module1/Home.5df6d51ae69c1517c7d4.umd.min.js')
+  // }
 ]
+routes.push(...ROUTES1)
 
 const router = new VueRouter({
   mode: 'history',
